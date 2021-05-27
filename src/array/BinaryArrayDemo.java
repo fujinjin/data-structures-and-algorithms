c2b63d0430ee98f553358369bbe623eef1e4f152
hale.fu <hale.fu@derbysoft.com>
2021-05-27 17:20:25 +0800

    m

--- src/array/BinaryArrayDemo.java
+++ src/array/BinaryArrayDemo.java
@@ -10,7 +10,27 @@ public class BinaryArrayDemo {
         for (int i = 0; i <a.length ; i++) {
             System.out.println(a[i]);
         }
+    }
 
-
+    public int getNum(int[]array, int target){
+        if (array ==null || array.length == 0){
+            return -1;
+        }
+        int low = 0;
+        int high = array.length;
+        int mid = low + (high - low)/2;
+        while (low<=high){
+            if (array[mid] == target){
+                while (mid>0 && array[mid-1]== array[mid]){
+                    mid--;
+                }
+                return mid-1;
+            }else if (array[mid] > target){
+                low = mid +1;
+            }else {
+                high = mid-1;
+            }
+        }
+        return -1;
     }
 }
